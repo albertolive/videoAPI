20d7adef856aa214995ab25cbd05c43332db8102
Albert Olivé <albert.olive@ideaknow.com>
2015-02-27 14:22:35 +0100

    api youtube funciona parcialment

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,3 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-// Parse.Cloud.define("hello", function(request, response) {
-//   response.success("Hello world!");
-// });
-
 Parse.Cloud.job('video', function(request, status) {
 	Parse.Cloud.httpRequest({
 	  url: 'https://www.googleapis.com/youtube/v3/activities/',
@@ -38,12 +31,35 @@ var myFunction = function(request){
  return Parse.Cloud.httpRequest({
 	  url: 'https://www.googleapis.com/youtube/v3/videos/',
 	  params: {
-	  	'part': 'statistics',
+	  	'part': 'statistics,snippet',
 	  	'id': request,
+	  	'maxResults': 50,
 	  	'key': 'AIzaSyAN4CHpB3th3FJ0hhhtI5YjGXyD4tw847k'
 	  },
 	  success: function(httpResponse) {
-	  	console.log("text: " + httpResponse.data.items[0].statistics.viewCount);
+	  	// console.log("Title: " + httpResponse.data.items[0].snippet.title);
+	  	// console.log("Views: " + httpResponse.data.items[0].statistics.viewCount);
+
+		var Youtube = Parse.Object.extend("youtube");
+		var saveArray = [];
+      	var youtube = new Youtube();
+
+      	youtube.set("title", httpResponse.data.items[0].snippet.title);
+      	youtube.set("publishedAt", httpResponse.data.items[0].snippet.publishedAt);
+  	  	youtube.set("plays", parseInt(httpResponse.data.items[0].statistics.viewCount));
+      	saveArray.push(youtube);
+
+      		Parse.Promise.when(saveArray).then(function() {
+		  		// save all the newly created objects
+			    Parse.Object.saveAll(saveArray, {
+			        success: function(objs) {
+			            alert('New object created with objectId: ' + youtube.id);
+			        },
+			        error: function(error) {
+			            alert('Failed to create new object, with error code: ' + error.message);
+			        }
+			    });
+  			});
 	  },
 	  error: function(httpResponse) {
 	    console.error('Request failed with response code ' + httpResponse.status);
