3d87d6e3ecdda2098f6d0d9c2f341b258beb8c6b
Albert Olivé <albert.olive@ideaknow.com>
2015-03-06 14:57:50 +0100

    Started working soundcloud

--- cloud/soundcloud.js
+++ cloud/soundcloud.js
@@ -7,49 +7,65 @@ Parse.Cloud.job('soundcloud', function(request, status) {
 	  	'client_id': '53af733efcdd4d7055d576217b95752a'
 	  },
 		success: function(httpResponse) {
-			// for (var i = 0; i < httpResponse.data.length; i++) {
-			// 	console.log(httpResponse.data[i]);
-			// };
-			_.each(httpResponse.data.length, function(httpResponse){
-				console.log(httpResponse.data);
+			var Soundcloud = Parse.Object.extend("soundcloud");
+			var prom = _.map(httpResponse.data, function(singleItem) {
+				//console.log(singleItem.created_at);
+				var soundcloudObject = new Soundcloud();
+				soundcloudObject.set('externalId', singleItem.id);
+				soundcloudObject.set('title', singleItem.title);
+				soundcloudObject.set('publishedAt', singleItem.created_at);
+				soundcloudObject.set('plays', parseInt(singleItem.playback_count));
+				soundcloudObject.save();
 			});
-			status.success();
-			// var query = new Parse.Query("soundcloud");
-			// query.get(request.object.id).then(function(soundCloudObject) {
-			//   	if(soundCloudObject.get('title') === undefined){
-			//   		soundCloudObject.set("title", httpResponse.data.items[0].snippet.title);
-		 //      		soundCloudObject.set("publishedAt", httpResponse.data.items[0].snippet.publishedAt);
-			//   	}
-		 //  	  	soundCloudObject.set("plays", parseInt(httpResponse.data.items[0].statistics.viewCount));
-		 //  		// save all the newly created objects
-			//     soundCloudObject.save();
-		 //    }
+			// for (var i = 0; i < httpResponse.data.length; i++) {
+
+			// 	var query = new Parse.Query("soundcloud");
+			// 	query.get(httpResponse.data[i].title).then(function(soundcloudObject) {
+			// 		console.log(soundcloudObject);
+			// 		var soundcloudObject = new Soundcloud();
+			// 		if (!foundedSoundcloud) {
+			// 			console.log("no esta");
+			// 			soundcloudObject.set("title", httpResponse.data[i].title);
+		 //      			soundcloudObject.set("publishedAt", httpResponse.data[i].created_at);
+			// 		}
+			// 		soundcloudObject.set("plays", parseInt(httpResponse.data[i].playback_count));
+			//   		// save all the newly created objects
+			// 	    return soundcloudObject.save();
+			// 	});
+			// };
+			Parse.Promise.when(prom).then(function() {
+		  		status.success();
+		  	});
 		  },
 		  error: function(httpResponse) {
+		  	status.error(httpResponse);
 		    console.error('Request failed with response code ' + httpResponse.status);
 		  }
 	});
 });
 
 
+				// var soundcloudObject = new Soundcloud();
+				// if(soundcloudObject.get('title') === undefined) {
+			 //  		soundcloudObject.set("title", httpResponse.data[i].title);
+		  //     		soundcloudObject.set("publishedAt", httpResponse.data[i].created_at);
+			 //  	}
+		  // 	  	soundcloudObject.set("plays", parseInt(httpResponse.data[i].playback_count));
+		  // 		// save all the newly created objects
+			 //    soundcloudObject.save();
 
+// Parse.Cloud.define('soundCloudPlays', function(request, request) {
+// 	var query = new Parse.Query("youtube");
+// 	query.find().then(function(youtubeCollection) {
+// 		var results = [];
+// 		_.each(youtubeCollection, function(singleYoutubeObject){
+// 			var obj = {
+// 				title: singleYoutubeObject.get('title'),
+// 				plays: singleYoutubeObject.get('plays')
+// 			}
+// 			results.push(obj);
+// 		});
 
-
-
-
-
-Parse.Cloud.define('soundCloudPlays', function(request, request) {
-	var query = new Parse.Query("youtube");
-	query.find().then(function(youtubeCollection) {
-		var results = [];
-		_.each(youtubeCollection, function(singleYoutubeObject){
-			var obj = {
-				title: singleYoutubeObject.get('title'),
-				plays: singleYoutubeObject.get('plays')
-			}
-			results.push(obj);
-		});
-
-		request.success(results);
-	});
-});
+// 		request.success(results);
+// 	});
+// });
