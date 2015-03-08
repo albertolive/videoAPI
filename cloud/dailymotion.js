d88070beaa6eb18b55f05d2c91409380fb40aa44
albert olive <albertolivecorbella@gmail.com>
2015-03-08 18:50:29 +0100

    dailymotion acabat

--- cloud/dailymotion.js
+++ cloud/dailymotion.js
@@ -2,11 +2,7 @@ var _ = require('underscore');
 
 Parse.Cloud.job('dailymotion', function(request, status) {
 	Parse.Cloud.httpRequest({
-	  url: 'https://api.dailymotion.com/user/djalbertolive/videos',
-	  params: {
-	  	'fields': 'id, title, views_total',
-	  	'sort': 'recent'
-	  },
+	  url: 'https://api.dailymotion.com/user/djalbertolive/videos?fields=id,title,views_total,&sort=recent&limit=100',
 		success: function(httpResponse) {
 			var Dailymotion = Parse.Object.extend("dailymotion");
 			var prom = _.map(httpResponse.data.list, function(singleItem) {
@@ -29,7 +25,30 @@ Parse.Cloud.job('dailymotion', function(request, status) {
 		  },
 		  error: function(httpResponse) {
 		  	status.error(httpResponse);
-		    console.error('Request failed with response code ' + httpResponse.status);
+		    console.error('Request first call failed with response code ' + httpResponse.status);
 		  }
+	});
+});
+
+Parse.Cloud.afterSave("dailymotion", function(request) {
+	var query = new Parse.Query("dailymotion");
+	query.get(request.object.id).then(function(dailymotionObject) {
+		Parse.Cloud.httpRequest({
+	 		url: 'https://api.dailymotion.com/user/djalbertolive/videos?fields=id,title,views_total,&sort=recent&limit=100',
+			success: function(httpResponse) {
+				_.map(httpResponse.data.list, function(singleItem) {
+					if (dailymotionObject.get('externalId') == singleItem.id) {
+						if(dailymotionObject.get('title') === undefined){
+							dailymotionObject.set('title', singleItem.title);
+						}
+							dailymotionObject.set('plays', parseInt(singleItem.views_total));
+							dailymotionObject.save();
+					}
+				});
+			},
+			error: function(httpResponse) {
+			    console.error('Request second call failed with response code ' + httpResponse.status);
+			}
+		});
 	});
 });
