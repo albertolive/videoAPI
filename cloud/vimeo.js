ed00eeafbe7da247ba42c6d1d87e8682ea647962
Albert Olive <albert.olive@ideaknow.com>
2015-03-19 10:51:54 +0100

    Finished vimeo

--- cloud/vimeo.js
+++ cloud/vimeo.js
@@ -2,34 +2,33 @@ var _ = require('underscore');
 
 Parse.Cloud.job('vimeo', function(request, status) {
 	Parse.Cloud.httpRequest({
-	  url: 'https://api.vimeo.com/me/videos',
+	  url: 'https://api.vimeo.com/users/17211520/videos',
 	  headers: {
-	    'Content-Type': 'application/vnd.vimeo.video+json'
+	    'Content-Type': 'application/json',
+	    'Accept': 'application/vnd.vimeo.video+json'
 	  },
 	  params: {
 	  	'access_token': '5e32ecbfff92f594092cefc20cb6c929',
-	  	'per_page': 1,
-	  	'sort': 'plays'
+	  	'per_page': '50'
 	  },
 	  success: function(httpResponse) {
-	  	console.log("prova " + httpResponse.text.total);
-	  	//var Vimeo = Parse.Object.extend("vimeo");
-		var prom = _.map(httpResponse.data, function(singleItem){
-			console.log("prova " + singleItem);
-			// var query = new Parse.Query("vimeo");
-		 //  	query.equalTo('externalId', singleItem.contentDetails.upload.videoId)
-			// return query.first().then(function(foundedYoutube) {
+	  	var getData = JSON.parse(httpResponse.text);
+	  	var Vimeo = Parse.Object.extend('vimeo');
 
-			// 	if(foundedYoutube){
-			// 		return foundedYoutube.save();
-			// 	}else{
-			// 		var vimeoObject = new Vimeo();
-			//   		youtubeObject.set('externalId', singleItem.contentDetails.upload.videoId);
-			// 		return youtubeObject.save();
-			// 	}
-			// });
+		var prom = _.map(getData.data, function(singleItem){
+			var query = new Parse.Query('vimeo');
+		  	query.equalTo('externalId', singleItem.uri)
+			return query.first().then(function(foundedVimeo) {
+
+				if(foundedVimeo){
+					return foundedVimeo.save();
+				}else{
+					var vimeoObject = new Vimeo();
+			  		vimeoObject.set('externalId', singleItem.uri);
+					return vimeoObject.save();
+				}
+			});
 		});
-
 	  	Parse.Promise.when(prom).then(function() {
 	  		status.success();
 	  	});
@@ -41,33 +40,40 @@ Parse.Cloud.job('vimeo', function(request, status) {
 	});
 });
 
-// Parse.Cloud.afterSave("vimeo", function(request) {
-// 		var query = new Parse.Query("youtube");
-// 		query.get(request.object.id).then(function(youtubeObject) {
+Parse.Cloud.afterSave("vimeo", function(request) {
+		var query = new Parse.Query('vimeo');
+		query.get(request.object.id).then(function(vimeoObject) {
 
-// 		Parse.Cloud.httpRequest({
-// 		  url: 'https://www.googleapis.com/youtube/v3/videos/',
-// 		  params: {
-// 		  	'part': 'statistics,snippet',
-// 		  	'id': youtubeObject.get('externalId'),
-// 		  	'maxResults': 50,
-// 		  	'key': 'AIzaSyAN4CHpB3th3FJ0hhhtI5YjGXyD4tw847k'
-// 		  },
-// 		  success: function(httpResponse) {
-// 		  	if(youtubeObject.get('title') === undefined){
-// 		  		youtubeObject.set("title", httpResponse.data.items[0].snippet.title);
-// 	      		youtubeObject.set("publishedAt", httpResponse.data.items[0].snippet.publishedAt);
-// 		  	}
-// 	  	  	youtubeObject.set("plays", parseInt(httpResponse.data.items[0].statistics.viewCount));
-// 	  		// save all the newly created objects
-// 		    youtubeObject.save();
-// 		  },
-// 		  error: function(httpResponse) {
-// 		    console.error('Request failed with response code ' + httpResponse.status);
-// 		  }
-// 		});
-// 	});
-// });
+		Parse.Cloud.httpRequest({
+		  url: 'https://api.vimeo.com/users/17211520/videos',
+		  headers: {
+		    'Content-Type': 'application/json',
+		    'Accept': 'application/vnd.vimeo.video+json'
+		  },
+		  params: {
+		  	'access_token': '5e32ecbfff92f594092cefc20cb6c929',
+		  	'per_page': '50'
+		  },
+		  success: function(httpResponse) {
+		  	var getData = JSON.parse(httpResponse.text);
+
+		  	_.map(getData.data, function(singleItem) {
+		  		if (vimeoObject.get('externalId') == singleItem.uri) {
+				  	if(vimeoObject.get('title') === undefined){
+				  		vimeoObject.set("title", singleItem.name);
+			      		vimeoObject.set("publishedAt", singleItem.created_time);
+				  	}
+			  	  	vimeoObject.set("plays", parseInt(singleItem.stats.plays));
+				    vimeoObject.save();
+			    }
+		    });
+		  },
+		  error: function(httpResponse) {
+		    console.error('Request failed with response code ' + httpResponse.status);
+		  }
+		});
+	});
+});
 
 // Parse.Cloud.define('vimeoPlays', function(request, request) {
 // 	var query = new Parse.Query("youtube");
